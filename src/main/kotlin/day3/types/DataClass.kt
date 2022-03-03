ab5de6dcafd30554199d9c1dc3c0575512719fb9
ItsFRZ <faraz.ar.sheikh@gmail.com>
2022-03-03 17:03:07 +0530

    Data class

---
+++ src/main/kotlin/day3/types/DataClass.kt
@@ -0,0 +1,20 @@
+package day3.types
+
+
+data class Wallpaper(var name : String, var resolution : String,var category: String)
+
+fun main(){
+
+    val(n,r,c) = Wallpaper("Audi","4k","Cars")
+    println("$n $r $c")
+    println()
+
+
+    val obj1 : Wallpaper = Wallpaper("Lykanhypersport","4k","Supercar")
+    val obj2 : Wallpaper = obj1.copy("Buggati Cheron")
+
+
+    println(obj1.toString())
+    println(obj2.toString())
+
+}
