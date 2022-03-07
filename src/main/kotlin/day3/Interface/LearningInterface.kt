421f007179b9dc534e67dd4c6db87d72db3fd27b
ItsFRZ <faraz.ar.sheikh@gmail.com>
2022-03-07 12:42:17 +0530

    Kotlin Interface

---
+++ src/main/kotlin/day3/Interface/LearningInterface.kt
@@ -0,0 +1,45 @@
+package day3.Interface
+
+
+interface MyClickListener{
+    // Properties in an interface should be abstract so that we cannot initialize
+    var name : String
+    val age : String
+
+    fun onClick(){
+        println("Button is Clicked")
+    }
+
+    fun onTouch()
+
+}
+
+interface MyDoubleClickListener{
+    fun onClick(){
+        println("Button is Clicked")
+    }
+
+    fun onTouch()
+}
+
+class Button() : MyClickListener,MyDoubleClickListener{
+
+    override var name : String = ""
+    override val age: String = ""
+
+
+    override fun onClick() {
+        super<MyClickListener>.onClick()
+    }
+
+    override fun onTouch() {
+        println("Button is Touched")
+    }
+
+}
+
+fun main(){
+    val btn : Button = Button()
+    btn.onClick()
+    btn.onTouch()
+}
