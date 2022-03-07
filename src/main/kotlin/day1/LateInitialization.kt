fc930dac08ce01493a30df427542d5dcb16a958c
ItsFRZ <faraz.ar.sheikh@gmail.com>
2022-03-07 19:22:46 +0530

    Lateinit variables

---
+++ src/main/kotlin/day1/LateInitialization.kt
@@ -0,0 +1,23 @@
+package day1
+
+
+class Person(){
+    lateinit var name: String
+    var age : Int? = null
+}
+
+fun main() {
+    val p1 = Person()
+    with(p1){
+
+        try{
+            println(age ?: 0)
+            println(name)
+
+        }catch(e : UninitializedPropertyAccessException){
+            println("lateinint property is not initialized")
+        }
+    }
+
+
+}
