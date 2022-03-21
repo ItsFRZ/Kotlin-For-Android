867e6a2c767ee836261377243a21be37f178da34
ItsFRZ <faraz.ar.sheikh@gmail.com>
2022-03-21 13:26:28 +0530

    Lazy Initialization

---
+++ src/main/kotlin/day11/otherconcepts/LazyConcept.kt
@@ -0,0 +1,22 @@
+package day11.otherconcepts
+
+
+val pi : Double by lazy{
+    3.14
+}
+
+val data : String by lazy{
+    "Faraz Sheikh"
+}
+
+
+fun data(){
+
+//    print("Some data")
+    print(data+" "+pi)
+
+}
+
+fun main() {
+    data()
+}
